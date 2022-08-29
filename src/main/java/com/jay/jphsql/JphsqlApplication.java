84f085c2641b17bd49c3f53b335c2607d56378dd
jailsonveiga <jmdaveiga2012@gmail.com>
2022-08-29 18:28:37 -0400

    Initial commit

---
+++ src/main/java/com/jay/jphsql/JphsqlApplication.java
@@ -0,0 +1,13 @@
+package com.jay.jphsql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JphsqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JphsqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jay/jphsql/JphsqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jay.jphsql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JphsqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
