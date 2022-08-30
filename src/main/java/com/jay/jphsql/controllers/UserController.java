42d1d760bba3bc014da7e4d44cb36ba49e07da5c
jailsonveiga <jmdaveiga2012@gmail.com>
2022-08-30 12:22:55 -0400

    Getting all user directly from JPH API

--- src/main/java/com/jay/jphsql/controllers/UserController.java
+++ src/main/java/com/jay/jphsql/controllers/UserController.java
@@ -1,4 +1,34 @@
 package com.jay.jphsql.controllers;
 
+import com.jay.jphsql.models.UserModel;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
+
+    private final String JPH_API_URL = "https://jsonplaceholder.typicode.com/users";
+
+    // getting all user directly from JPH API
+    @GetMapping("/jph/all")
+    public ResponseEntity<?> getAllUsersAPI (RestTemplate restTemplate) {
+
+        try {
+
+            UserModel[] allUsers = restTemplate.getForObject(JPH_API_URL, UserModel[].class);
+
+            return ResponseEntity.ok(allUsers);
+
+        } catch (Exception e) {
+            System.out.println(e.getClass());
+            System.out.println(e.getMessage());
+
+            return ResponseEntity.internalServerError().body(e.getMessage());
+        }
+    }
+
 }
