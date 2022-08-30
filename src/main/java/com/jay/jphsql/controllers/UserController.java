40c573eae1a18fb364531404cdc19fcab65a62f6
jailsonveiga <jmdaveiga2012@gmail.com>
2022-08-30 12:50:19 -0400

    Get all users stored in our local MySQL database

--- src/main/java/com/jay/jphsql/controllers/UserController.java
+++ src/main/java/com/jay/jphsql/controllers/UserController.java
@@ -1,17 +1,24 @@
 package com.jay.jphsql.controllers;
 
 import com.jay.jphsql.models.UserModel;
+import com.jay.jphsql.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
+
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
 
     private final String JPH_API_URL = "https://jsonplaceholder.typicode.com/users";
+
+    @Autowired
+    private UserRepository userRepository;
 
     // getting all user directly from JPH API
     @GetMapping("/jph/all")
@@ -20,6 +27,23 @@ public class UserController {
         try {
 
             UserModel[] allUsers = restTemplate.getForObject(JPH_API_URL, UserModel[].class);
+
+            return ResponseEntity.ok(allUsers);
+
+        } catch (Exception e) {
+            System.out.println(e.getClass());
+            System.out.println(e.getMessage());
+
+            return ResponseEntity.internalServerError().body(e.getMessage());
+        }
+    }
+
+    // get all users stored in our local MySQL database
+    @GetMapping("/sql/all")
+    public ResponseEntity<?> getAllUsersSQL () {
+        try {
+
+            ArrayList<UserModel> allUsers = (ArrayList<UserModel>) userRepository.findAll();
 
             return ResponseEntity.ok(allUsers);
 
