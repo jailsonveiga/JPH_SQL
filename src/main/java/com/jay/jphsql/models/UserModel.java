4e3053379599d5936c001090a77791c95544dd7c
jailsonveiga <jmdaveiga2012@gmail.com>
2022-08-30 11:41:26 -0400

    Adding Fields and Getters

--- src/main/java/com/jay/jphsql/models/UserModel.java
+++ src/main/java/com/jay/jphsql/models/UserModel.java
@@ -1,4 +1,43 @@
 package com.jay.jphsql.models;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "User")
 public class UserModel {
+
+    @Id
+    private int id;
+
+    private String name;
+    private String username;
+    private String email;
+    private String phone;
+    private String website;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
 }
