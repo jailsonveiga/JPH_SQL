7c464ea00cc5bf8b63da3490d3c9b6883b78e91d
jailsonveiga <jmdaveiga2012@gmail.com>
2022-09-01 11:53:08 -0400

    Get all users stored in our local MySQL database

--- src/main/java/com/jay/jphsql/controllers/CommentController.java
+++ src/main/java/com/jay/jphsql/controllers/CommentController.java
@@ -1,4 +1,36 @@
 package com.jay.jphsql.controllers;
 
+import com.jay.jphsql.models.CommentModel;
+import com.jay.jphsql.models.UserModel;
+import com.jay.jphsql.repositories.CommentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+
+@RestController
 public class CommentController {
+
+    @Autowired
+    private CommentRepository commentRepository;
+
+    // get all users stored in our local MySQL database
+    @GetMapping("/sql/all")
+    public ResponseEntity<?> getAllCommentsSQL () {
+        try {
+
+            ArrayList<CommentModel> allComments= (ArrayList<CommentModel>) commentRepository.findAll();
+
+            return ResponseEntity.ok(allComments);
+
+        } catch (Exception e) {
+            System.out.println(e.getClass());
+            System.out.println(e.getMessage());
+
+            return ResponseEntity.internalServerError().body(e.getMessage());
+        }
+    }
 }
