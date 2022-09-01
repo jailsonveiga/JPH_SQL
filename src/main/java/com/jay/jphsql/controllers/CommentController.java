2889b53a3681ddd55927002cdb9d8977807b548d
jailsonveiga <jmdaveiga2012@gmail.com>
2022-09-01 12:51:05 -0400

    Get all the comments stored in our database

--- src/main/java/com/jay/jphsql/controllers/CommentController.java
+++ src/main/java/com/jay/jphsql/controllers/CommentController.java
@@ -7,12 +7,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 @RestController
+@RequestMapping("/api/comments")
 public class CommentController {
+
+    private final String JPH_API_URL = "https://jsonplaceholder.typicode.com/comments";
 
     @Autowired
     private CommentRepository commentRepository;
@@ -24,6 +30,35 @@ public class CommentController {
 
             ArrayList<CommentModel> allComments= (ArrayList<CommentModel>) commentRepository.findAll();
 
+            return ResponseEntity.ok(allComments);
+
+        } catch (Exception e) {
+            System.out.println(e.getClass());
+            System.out.println(e.getMessage());
+
+            return ResponseEntity.internalServerError().body(e.getMessage());
+        }
+    }
+
+    @PostMapping ("/sql/all")
+    public ResponseEntity<?> uploadAllCommentsSQL (RestTemplate restTemplate) {
+        try {
+
+            // retrieve data from JPH API and save to array of comments
+            CommentModel[] allComments = restTemplate.getForObject(JPH_API_URL, CommentModel[].class);
+
+            // check that allComments is present, otherwise an exception will be thrown
+            assert allComments != null;
+
+            // remove id from each comment
+            for(CommentModel comment : allComments) {
+                comment.removeId();
+            }
+
+            // saves comment to database and updates each comment's id to the saved database ID
+            commentRepository.saveAll(Arrays.asList(allComments));
+
+            // respond with the data that was just saved to the database
             return ResponseEntity.ok(allComments);
 
         } catch (Exception e) {
