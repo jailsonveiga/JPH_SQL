5cd9449c4db98a431ae2268096e698aba0bb5e0f
jailsonveiga <jmdaveiga2012@gmail.com>
2022-09-01 11:42:40 -0400

    Add properties "Fields name" and generate Getters

--- src/main/java/com/jay/jphsql/models/CommentModel.java
+++ src/main/java/com/jay/jphsql/models/CommentModel.java
@@ -1,4 +1,39 @@
 package com.jay.jphsql.models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Comment")
 public class CommentModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+
+    private int id;
+
+    private int postId;
+
+    private String name;
+    private String email;
+    private String body;
+
+    public int getId() {
+        return id;
+    }
+
+    public int getPostId() {
+        return postId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getBody() {
+        return body;
+    }
 }
